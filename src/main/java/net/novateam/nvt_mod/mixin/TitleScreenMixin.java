a28e4ba9adcba0da6982f5fab4a1ee881fe839f3
alguewator <avato999@gmail.com>
2024-10-10 17:40:11 -0400

    Changed all "exemple mod" occurence to "NVT"

--- src/main/java/net/novateam/nvt_mod/mixin/TitleScreenMixin.java
+++ src/main/java/net/novateam/nvt_mod/mixin/TitleScreenMixin.java
@@ -1,7 +1,8 @@
-package com.example.example_mod.mixin;
+package net.novateam.nvt_mod.mixin;
 
-import com.example.example_mod.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.novateam.nvt_mod.NVTMod;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		NVTMod.LOGGER.info("This line is printed by an example mod (now NVT MOD) mixin!");
 	}
 }
